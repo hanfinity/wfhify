43d9c944c47508dc9bd08df091ed512f84d21866
hanfinity <hanmic@pdx.edu>
2022-05-25 13:25:27 -0700

    update host address

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -85,7 +85,7 @@ public class Server {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    createAndShowGUI(Inet4Address.getLocalHost().getCanonicalHostName());
+                    createAndShowGUI(Inet4Address.getLocalHost().getHostAddress());
                 } catch (UnknownHostException e) {
                     e.printStackTrace();
                 }
